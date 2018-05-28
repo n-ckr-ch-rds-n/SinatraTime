45a0aa66371cd84ac123d42951bcc6de5776e82e
Nick <nicholasjohnrichardson@gmail.com>
2018-05-28 22:53:39 +0100

    added cat route and img

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
 require 'sinatra'
+require 'shotgun'
+set :session_secret, 'super secret'
 
-get '/' do
-  "Hello!"
-end
-
-get '/secret' do
-  "This is a secret page!"
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+  <img src='http://bit.ly/1eze8aE'>
+  </div>"
 end
