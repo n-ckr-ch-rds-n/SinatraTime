e6fe91d6c5cd4660c971a8d6b89358130a80e9db
Nick <nicholasjohnrichardson@gmail.com>
2018-05-28 23:34:24 +0100

    playing with params

--- app.rb
+++ app.rb
@@ -2,7 +2,13 @@ require 'sinatra'
 require 'shotgun'
 set :session_secret, 'super secret'
 
-get '/cat' do
-  @names = ["Amigo", "Oscar", "Viking"].sample
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
